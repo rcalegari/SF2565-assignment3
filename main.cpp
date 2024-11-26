e3fc9ecf4ec6868d8e846c0786c3bb4f7262e9f3
rcalegari <rcalegari@student.tudelft.nl>
2024-11-26 13:41:42 +0100

    clean main

--- main.cpp
+++ main.cpp
@@ -4,32 +4,19 @@
 // #include "utilities/point.h"
 
 int main() {
-    Point<double> p0 = {-10, 0};
-    Point<double> p1 = {5, 0};
-
-    StraightLine line(p0, p1);
-    line.print();
-
-    // Check if at function works
-    Point<double> p_mid = line.at(0.5);  // Midpoint
-    std::cout << "Midpoint: (" << p_mid.x << ", " << p_mid.y << ")" << std::endl;
-    Point<double> p_start = line.at(0.0);  // Start point
-    std::cout << "Start point: (" << p_start.x << ", " << p_start.y << ")" << std::endl;
-    Point<double> p_end = line.at(1.0);  // End point
-    std::cout << "End point: (" << p_end.x << ", " << p_end.y << ")" << std::endl;
 
     // check if BottomCurve implementation works
     BottomCurve bottom;
     const std::string filename = "bottom_curve_coordinates.txt";
     bottom.writeCoordinatesToFile(filename);
 
-    // check for default constructors
+    // check for default domain
     Domain domain;
     int n = 10;
     Grid grid(n);
     grid.createGrid(domain, "xGrid.txt", "yGrid.txt");
 
-    // check for self-defined domain
+    // check for user-defined domain
     Point<double> pp1 = {-5, 7};
     Point<double> pp2 = {4, 5};
     Point<double> pp3 = {-3, 11};
